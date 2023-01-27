07dbc7f67120550ba62d492f2ab51b1043d169cc
ALEXDRUGS <105850199+ALEXDRUGS@users.noreply.github.com>
2023-01-27 20:08:09 +0300

    TestCleanCode

--- src/UserTest.java
+++ src/UserTest.java
@@ -4,53 +4,48 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class UserTest {
+    private final User user = new User("login", "blabla@gmail.com");
+    private final User emptyUser = new User();
 
     @Test
     @DisplayName("Проверка правильного заполнения поля login при создании объекта")
     void getLoginTest() {
-        User user = new User("login", "blabla@gmail.com");
         assertEquals("login", user.getLogin());
     }
 
     @Test
     @DisplayName("Проверка правильного заполнения поля email при создании объекта")
     void getEmailTest() {
-        User user = new User("login", "blabla@gmail.com");
         assertEquals("blabla@gmail.com", user.getEmail());
     }
 
     @Test
     @DisplayName("Проверка пустого заполнения поля login при создании объекта")
     void getEmptyLoginTest() {
-        User user = new User();
-        assertNull(user.getLogin());
+        assertNull(emptyUser.getLogin());
     }
 
     @Test
     @DisplayName("Проверка пустого заполнения поля email при создании объекта")
     void getEmptyEmailTest() {
-        User user = new User();
-        assertNull(user.getEmail());
+        assertNull(emptyUser.getEmail());
     }
 
     @Test
     @DisplayName("Проверка наличия символа '@' в email")
-    public void isAtInEmailTest() {
-        User user = new User("login", "blabla@gmail.com");
+    void isAtInEmailTest() {
         assertTrue(user.getEmail().contains("@"));
     }
 
     @Test
     @DisplayName("Проверка наличия символа '.' в email")
-    public void isDotInEmailTest() {
-        User user = new User("login", "blabla@gmail.com");
+    void isDotInEmailTest() {
         assertTrue(user.getEmail().contains("."));
     }
 
     @Test
     @DisplayName("Проверка сравнения полей login и email при создании объекта")
     void comparisonLoginAndEmailTest() {
-        User user = new User("login", "blabla@gmail.com");
         assertNotEquals(user.getLogin(), user.getEmail());
     }
 }
